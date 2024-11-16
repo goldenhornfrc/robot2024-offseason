10e8a963e113773b9e9ca911bf65daab0e058630
Kerem Alp <keremalpakkaya0508@gmail.com>
2024-11-16 03:18:34 +0300

    Add ShooterPivotSubsystem

--- src/main/java/frc/robot/subsystems/shooterPivot/ShooterPivotIO.java
+++ src/main/java/frc/robot/subsystems/shooterPivot/ShooterPivotIO.java
@@ -1,8 +1,28 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems.shooterPivot;
 
+import org.littletonrobotics.junction.AutoLog;
+
 /** Add your docs here. */
-public class ShooterPivotIO {}
+public interface ShooterPivotIO {
+
+  @AutoLog
+  public static class ShooterPivotIOInputs {
+    public double shooterPosition = 0;
+    public double pivotVolts = 0;
+  }
+
+  public default void setVoltage(double voltage) {}
+
+  public default void setPivotAngle(double targetAngle) {}
+
+  public default double getShooterPivotAngle() {
+    return 0;
+  }
+
+  public default double getTargetAngle() {
+    return 0;
+  }
+  public default void updateInputs(ShooterPivotIOInputs inputs) {}
+}
--- src/main/java/frc/robot/subsystems/shooterPivot/ShooterPivotSubsystem.java
+++ src/main/java/frc/robot/subsystems/shooterPivot/ShooterPivotSubsystem.java
@@ -4,14 +4,59 @@
 
 package frc.robot.subsystems.shooterPivot;
 
+import org.littletonrobotics.junction.Logger;
+
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.networktables.StructPublisher;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ShooterPivotSubsystem extends SubsystemBase {
-  /** Creates a new ShooterPivotSubsystem. */
-  public ShooterPivotSubsystem() {}
+  private final ShooterPivotIO io;
+  private final ShooterPivotIOInputsAutoLogged inputs = new ShooterPivotIOInputsAutoLogged();
+
+  private final Translation3d pivotPosition = new Translation3d(0, 0.05, 0.63);
+
+  StructPublisher<Pose3d> publisher =
+      NetworkTableInstance.getDefault().getStructTopic("MyPose", Pose3d.struct).publish();
+
+
+  public ShooterPivotSubsystem(ShooterPivotIO io) {
+    this.io = io;
+  }
+
+
+  public void setVoltage(double voltage) {
+    io.setVoltage(voltage);
+  }
+
+
+  public void setArmAngle(double targetAngle) {
+    io.setPivotAngle(targetAngle);
+  }
+
+  public double getShooterPivotAngle() {
+    return io.getShooterPivotAngle();
+  }
+
+  public double getTargetAngle() {
+    return io.getTargetAngle();
+  }
+
+  public double getPosition() {
+    return inputs.shooterPosition;
+  }
 
   @Override
   public void periodic() {
+    io.updateInputs(inputs);
+
+
+    Logger.processInputs("Pivot", inputs);
+
+    publisher.set(new Pose3d(pivotPosition, new Rotation3d(io.getShooterPivotAngle(), 0, 0)));
     // This method will be called once per scheduler run
   }
 }
