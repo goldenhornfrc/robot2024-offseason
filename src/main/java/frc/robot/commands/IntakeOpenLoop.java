91ed0835e3b416f36c101be8150a55a851a2aeb9
KeremAlp01 <keremalpakkaya0508@gmail.com>
2024-11-18 23:44:27 +0300

    Revert "Add Open Loop Commands"
    This reverts commit d5d393dc49c539998dcefb5aa2fcd6db581aa44e.

--- src/main/java/frc/robot/commands/IntakeOpenLoop.java
+++ src/main/java/frc/robot/commands/IntakeOpenLoop.java
@@ -2,23 +2,22 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.OpenLoop;
+package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.shooter.ShooterSubsystem;
+import frc.robot.subsystems.intake.IntakeSubsystem;
 
-public class ShooterOpenLoop extends Command {
-  /** Creates a new ShooterOpenLoop. */
-  private ShooterSubsystem m_shooter;
+public class IntakeOpenLoop extends Command {
+  /** Creates a new IntakeOpenLoop. */
+  private IntakeSubsystem m_intake;
 
   private double m_voltage;
 
-  public ShooterOpenLoop(ShooterSubsystem shooter, double voltage) {
+  public IntakeOpenLoop(IntakeSubsystem intake, double voltage) {
     // Use addRequirements() here to declare subsystem dependencies.
-    m_shooter = shooter;
+    m_intake = intake;
     m_voltage = voltage;
-
-    addRequirements(m_shooter);
+    addRequirements(m_intake);
   }
 
   // Called when the command is initially scheduled.
@@ -28,13 +27,13 @@ public class ShooterOpenLoop extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    m_shooter.setVoltage(m_voltage, m_voltage);
+    m_intake.setVoltage(m_voltage);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
-    m_shooter.setVoltage(0, 0);
+    m_intake.setVoltage(0);
   }
 
   // Returns true when the command should end.
