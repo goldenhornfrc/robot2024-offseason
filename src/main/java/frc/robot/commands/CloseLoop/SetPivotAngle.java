91f1ddfe7d8a9e87453f974bd3e4d3f0231070c9
KeremAlp01 <keremalpakkaya0508@gmail.com>
2024-11-18 23:43:17 +0300

    Add SetPivotAngle

---
+++ src/main/java/frc/robot/commands/CloseLoop/SetPivotAngle.java
@@ -0,0 +1,53 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.CloseLoop;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.shooterPivot.ShooterPivotSubsystem;
+
+public class SetPivotAngle extends Command {
+  private final ShooterPivotSubsystem shooterPivot;
+  private final double targetAngle;
+  private final boolean shouldHold;
+
+  /** Creates a new SetPivotAngle. */
+  public SetPivotAngle(ShooterPivotSubsystem shooterPivot, double targetAngle, boolean shouldHold) {
+    this.shooterPivot = shooterPivot;
+    this.targetAngle = targetAngle;
+    this.shouldHold = shouldHold;
+    addRequirements(shooterPivot);
+    // Use addRequirements() here to declare subsystem dependencies.
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    if (Math.abs(shooterPivot.getShooterPivotAngle() - targetAngle) <= 5) {
+      shooterPivot.setPositionVoltageAngle(targetAngle);
+    } else {
+      shooterPivot.setMotionMagicAngle(targetAngle);
+    }
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    shooterPivot.setVoltage(0);
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    if (!shouldHold) {
+      return Math.abs(shooterPivot.getShooterPivotAngle() - targetAngle) <= 1;
+    } else {
+      return false;
+    }
+  }
+}
